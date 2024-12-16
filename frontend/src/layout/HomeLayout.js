fae08c5995070e420148ec9cb58d6a6f691d38fb
anujalamsal <lanuja667@gmail.com>
2024-12-16 23:11:18 +0545

    Count Down Working On

--- frontend/src/layout/HomeLayout.js
+++ frontend/src/layout/HomeLayout.js
@@ -1,28 +1,30 @@
 import {NavLink,Outlet} from 'react-router-dom';
 import {useState} from 'react';
 import profile from '../assets/home_profile.jpg';
+import './HomeLayout.css'
 
 export default function HomeLayout(){
     
-    const [timer,setTimer]=useState("00:00:00");
+    const [hours,setHour]=useState(0);
+    const [minutes,setMinute]=useState(0);
+    const [seconds,setSecond]=useState(0);
+
     
     
     return(
         <div className='root-layout'>
             <header>
-                <nav>
-                    <NavLink to="/home">Home</NavLink>
-                    <NavLink to="/home/kyc">Know Your Candidates</NavLink>
-                    <NavLink to="/home/result">Results</NavLink>
-                    <NavLink to="/home/guideline">Guidelines</NavLink>
+                <nav className='homeNavBar'>
+                    <NavLink to="/home" className={({isActive})=>(isActive?"active":"")}>Home</NavLink>
+                    <NavLink to="/home/kyc" className={({isActive})=>(isActive?"active":"")}>Know Your Candidates</NavLink>
+                    <NavLink to="/home/result" className={({isActive})=>(isActive?"active":"")}>Results</NavLink>
+                    <NavLink to="/home/guidelines" className={({isActive})=>(isActive?"active":"")}>Guidelines</NavLink>
+                    <button><img src={profile} alt='Voters Profile'/></button>
                 </nav>
             </header>
             <main>
-                <nav>
-                    <img src={profile} alt='Voters Profile'/>
-
-
-
+                <nav className='timerNavBar'>
+                    <h1>Ends In:</h1>
 
                 </nav>
                 <section>
